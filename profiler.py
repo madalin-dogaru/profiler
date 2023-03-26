bd8fb2a8cfdcb11f675a6ff56e8ecc8312bc1d0f
Mădălin Dogaru <100802803+draco-putin-slayer@users.noreply.github.com>
2023-03-27 01:36:52 +0300

    added comments

--- profiler.py
+++ profiler.py
@@ -7,16 +7,19 @@ import os
 import re
 from termcolor import colored
 
+# Custom ArgumentParser class with colored error messages
 class ColoredArgumentParser(argparse.ArgumentParser):
     def error(self, message):
         self.print_usage(sys.stderr)
         self.exit(2, "%s: error: %s\n" % (colored(self.prog, "red"), colored(message, "red")))
 
+# Class for generating various email formats from firstname and lastname pairs
 class EmailGenerator:
     def __init__(self, firstname_lastname_list, domain):
         self.firstname_lastname_list = firstname_lastname_list
         self.domain = domain
 
+    # Generate a list of possible email formats for each user
     def generate_emails(self, users):
         emails = []
         for firstname, lastname in users:
@@ -33,7 +36,8 @@ class EmailGenerator:
             emails.append(f'{firstname}@{self.domain}')
             emails.append(f'{lastname}@{self.domain}')
         return emails
-
+    
+    # Process the list of firstnames and lastnames, and generate emails
     def process_name_list(self, output_handle):
         users = []
         with open(self.firstname_lastname_list, "r") as name_file:
@@ -47,13 +51,16 @@ class EmailGenerator:
                 output_handle.write(email + "\n")
                 output_handle.flush()
 
+# Class for scanning a folder and extracting URLs from its files
 class URLScanner:
     def __init__(self, folder_path):
         self.folder_path = folder_path
-
+    
+    # Extract URLs from a given text using regex
     def extract_urls(self, text):
         return re.findall(r'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\\(\\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', text)
 
+    # Scan folder and extract URLs from its files
     def scan_folder(self):
         urls = []
 
@@ -67,10 +74,12 @@ class URLScanner:
 
         return urls
 
+# Class for getting information about IP addresses
 class IPInfo:
     def __init__(self, output_handle):
         self.output_handle = output_handle
 
+    # Get IP information using the ip-api.com API
     def get_ip_info(self, ip):
         response = requests.get(f"http://ip-api.com/json/{ip}")
         data = json.loads(response.content)
@@ -82,6 +91,7 @@ class IPInfo:
             print(colored(f"Failed to retrieve information for IP: {ip}", "red"))
             return None
 
+    # Process a list of IP addresses and get their information
     def process_ip_list(self, input_file):
         with open(input_file, "r") as ip_file:
             for line in ip_file:
@@ -92,11 +102,12 @@ class IPInfo:
                     if self.output_handle:
                         self.output_handle.write(result + "\n")
                         self.output_handle.flush()
-
+# Class for getting information about domain names
 class DomainInfo:
     def __init__(self, output_handle):
         self.output_handle = output_handle
 
+    # Get the IP address of a domain name
     def get_domain_ip(self, domain):
         try:
             ip = socket.gethostbyname(domain)
@@ -105,6 +116,7 @@ class DomainInfo:
             print(colored(f"Failed to resolve the domain: {domain}", "red"))
             return None
 
+    # Process a list of domain names and get their information
     def process_domain_list(self, domain_list):
         with open(domain_list, "r") as domain_file:
             for line in domain_file:
@@ -117,7 +129,7 @@ class DomainInfo:
                         if self.output_handle:
                             self.output_handle.write(result + "\n")
                             self.output_handle.flush()
-
+# Function for parsing command line arguments
 def parse_arguments():
     parser = ColoredArgumentParser(description='Get IP information and save to a file.')
     parser.add_argument('-iplist', help='Path to the file containing the list of IP addresses.')
@@ -130,6 +142,7 @@ def parse_arguments():
     parser.add_argument('-edom', help='The email domain to be used for generating email addresses.')
     return parser.parse_args()
 
+# Function for processing URL scans
 def process_url_scan(folder_path, output_handle):
     scanner = URLScanner(folder_path)
     urls = scanner.scan_folder()
@@ -140,6 +153,7 @@ def process_url_scan(folder_path, output_handle):
             output_handle.write(url + "\n")
             output_handle.flush()
 
+# Main function for handling command line arguments and processing input
 def main():
     args = parse_arguments()
     input_file = args.iplist
@@ -152,6 +166,8 @@ def main():
     email_domain = args.edom
 
     output_handle = open(output_file, "w") if output_file else None
+
+    # Process input based on the provided command line arguments
     if firstname_lastname_list and email_domain:
         email_generator = EmailGenerator(firstname_lastname_list, email_domain)
         email_generator.process_name_list(output_handle)
@@ -185,6 +201,7 @@ def main():
     else:
         print(colored("Please provide either a single IP (-ip), a domain (-d), a list of IPs (-iplist), a list of domains (-dlist), or a folder to scan for URLs (-url).", "yellow"))
 
+    # Close output_handle if it was opened
     if output_handle:
         output_handle.close()
 
