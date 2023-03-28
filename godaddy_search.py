2e41e8e8f4d9332cf25a1a118e1c7b5f68204344
Mădălin Dogaru <100802803+draco-putin-slayer@users.noreply.github.com>
2023-03-28 23:53:00 +0300

    test

--- godaddy_search.py
+++ godaddy_search.py
@@ -1,70 +1,50 @@
 import itertools
 import requests
-import random
+from typing import List
 
-def character_substitution(word):
-    substitutions = {
-        'o': '0',
-        'l': 'L',
-        'i': '1',
-        
-    }
-    new_word = ''
-    for char in word:
-        new_char = substitutions.get(char, char)
-        new_word += new_char if random.random() < 0.5 else char
-    return new_word
+api_key = 'gHVWzYDGHTs5_PJ63J1MtGbZmxZoxcb2iyK'
+api_secret = 'FDqRjsyoSor5n1M8ypNFfS'
 
-def character_insertion(word):
-    insertions = '0123456789'
-    index = random.randint(0, len(word))
-    return word[:index] + random.choice(insertions) + word[index:]
+substitutions = {
+    'o': '0',
+    'l': 'I',
+    'i': '1',
 
-def generate_similar_domains(domain, num_domains=20):
-    base_domain = domain.split(".")[0]
-    tlds = [".com", ".net", ".org", ".io", ".co"]
-    similar_domains = set()
+}
 
-    while len(similar_domains) < num_domains:
-        for tld in tlds:
-            altered_domain = base_domain
-            if random.random() < 0.5:
-                altered_domain = character_substitution(altered_domain)
-            if random.random() < 0.5:
-                altered_domain = character_insertion(altered_domain)
+def query_similar_domains(domain: str):
+    similar_domains = generate_similar_domains(domain, num_domains=20)
+    check_availability(similar_domains)
 
-            similar_domain = altered_domain + tld
-            if similar_domain != domain:
-                similar_domains.add(similar_domain)
-                if len(similar_domains) >= num_domains:
-                    break
+def generate_similar_domains(domain: str, num_domains: int = 10) -> List[str]:
+    domain_variation = character_substitution(domain)
+    
+    domains = [domain_variation]
 
-    return similar_domains
+    while len(domains) < num_domains:
+        new_domain = character_substitution(domain)
+        if new_domain not in domains:
+            domains.append(new_domain)
 
-def check_domain_availability(domain):
-    api_key = 'gHVWzYDGHTs5_PJ63J1MtGbZmxZoxcb2iyK'
-    api_secret = 'FDqRjsyoSor5n1M8ypNFfS'
+    return domains
 
+def character_substitution(word: str) -> str:
+    last_two_chars = word[-2:]
+    for original, replacement in substitutions.items():
+        last_two_chars = last_two_chars.replace(original, replacement)
+    return word[:-2] + last_two_chars
+
+def check_availability(domains: List[str]):
     headers = {
         'Authorization': f'sso-key {api_key}:{api_secret}',
     }
 
-    url = f'https://api.godaddy.com/v1/domains/available?domain={domain}'
+    for domain in domains:
+        url = f'https://api.godaddy.com/v1/domains/available?domain={domain}'
+        response = requests.get(url, headers=headers)
 
-    response = requests.get(url, headers=headers)
-
-    if response.status_code == 200:
-        domain_info = response.json()
-        return domain_info['available']
-    else:
-        return None
-
-def query_similar_domains(domain):
-    similar_domains = generate_similar_domains(domain)
-
-    for domain in similar_domains:
-        is_available = check_domain_availability(domain)
-        if is_available is None:
-            print(f"Error occurred while checking {domain}")
+        if response.status_code == 200:
+            domain_info = response.json()
+            print(f"{domain_info['domain']} is {'available' if domain_info['available'] else 'not available'}")
         else:
-            print(f"{domain} is {'available' if is_available else 'not available'}")
+            print(f"Error occurred: {response.status_code} - {response.text}")
