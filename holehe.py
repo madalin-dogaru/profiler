4255c4ce7f66731e8100804f5ee31884f158d154
shamo0 <genuka10@gmail.com>
2023-06-19 21:49:06 +0400

    added csv output support for mails

--- holehe.py
+++ holehe.py
@@ -12,6 +12,7 @@ Full props go to Palenath for an awesome tool: https://github.com/megadose/holeh
 """
 
 import subprocess
+import csv
 #import time
 
 class EmailProfiler:
@@ -36,7 +37,7 @@ class EmailProfiler:
 
             # Decode the output, split it into lines and filter out lines starting with '100%'
             output = process2.communicate()[0].decode('utf-8')
-            lines = output.split('\n')
+            lines = output.split('\n')          
 
             # Define the line to exclude
             exclude_line = "[+] Email used, [-] Email not used, [x] Rate limit, [!] Error"
@@ -48,3 +49,71 @@ class EmailProfiler:
             print('\n'.join(lines))
 
             #time.sleep(5)  # Wait for 5 seconds
+
+class EmailProfilerCSV:
+    def __init__(self, filename, csvname):
+        self.filename = filename
+        self.csvname = csvname
+
+    def profileCSV(self):
+        # Open file and read lines
+        with open(self.filename, 'r') as file:
+            emails = file.readlines()
+
+        # For each email in the list
+        results = {}
+
+        for email in emails:
+            
+            email = email.strip()  # remove leading/trailing whitespace and newlines
+
+            print(f"Results for {email}:")  # Print email before running holehe
+
+            # Run the command and pipe its output to grep
+            process1 = subprocess.Popen(['holehe', email], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            process2 = subprocess.Popen(['grep', '-F', '[+]'], stdin=process1.stdout, stdout=subprocess.PIPE)
+            process1.stdout.close()  # Allow process1 to receive a SIGPIPE if process2 exits
+
+            # Decode the output, split it into lines and filter out lines starting with '100%'
+            output = process2.communicate()[0].decode('utf-8')
+            lines = output.split('\n')
+ 
+
+            # Define the line to exclude
+            exclude_line = "[+] Email used, [-] Email not used, [x] Rate limit, [!] Error"
+
+            # Filter out the excluded line
+            lines = [line for line in lines if not line.startswith('100%') and line != exclude_line]
+            domains = []
+            for line in lines:
+                domain = line.split()
+                try:
+                    domains.append(domain[1])
+                except:
+                    continue
+            # Print the remaining lines
+            print('\n'.join(lines))
+
+            # add email and domains to results dictionary
+            results[email] = domains
+            #time.sleep(5)  # Wait for 5 seconds
+
+        header = ['email'] + sorted(set(domain for domains in results.values() for domain in domains))
+        rows = []
+        # create rows and colums for found domains and emails
+        for email, domains in results.items():
+            row = [email]
+            for domain in header[1:]:
+                if domain in domains:
+                    row.append('x')
+                else:
+                    row.append('')
+            rows.append(row)
+
+        #Write to a csv file
+        with open(self.csvname, 'w', newline='') as csvfile:
+            writer = csv.writer(csvfile)
+            writer.writerow(header)
+            writer.writerows(rows)
+
+        print(f'CSV file created: {self.csvname}')
